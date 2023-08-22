a7f160651abd3e9da4cbd24428fc1453bbe71265
bvmbo <wiktorsz2001@wp.pl>
2023-08-22 16:32:20 +0200

    created extra file for arrays

---
+++ js/arrays.js
@@ -0,0 +1,121 @@
+const questionsMobile = [
+	{
+		question: "Jaki projekt chcesz stworzyć?",
+		answers: [
+			{ text: "Aplikacja mobilna" },
+			{ text: "Aplikacja VR" },
+			{ text: "Aplikacja AR" },
+			{ text: "Gra" },
+			{ text: "Modelowanie 3D" },
+			{ text: "Web" },
+			{ text: "Jeszcze nie wiem" },
+			{ text: "Inny" },
+		],
+	},
+	{
+		question: "Na jaką platformę ma być przeznaczona aplikacja?",
+		answers: [{ text: "iOS" }, { text: "Android" }],
+	},
+	{
+		question: "Na jakim etapie jesteś?",
+		answers: [
+			{ text: "Robię research rynku" },
+			{ text: "Chce wybrać wykonawcę" },
+			{ text: "Mam wstępną koncepcję" },
+			{ text: "Mam całą kocepcję" },
+			{ text: "Nie mam jeszcze pomysłu" },
+			{ text: "Innym" },
+		],
+	},
+	{
+		question: "Do kogo kierowana jest aplikacja?",
+		answers: [
+			{ text: "Do klientów indywidualnych (B2C)" },
+			{ text: "Do klientów biznesowych (B2B)" },
+			{ text: "Do sektora edukacji" },
+			{ text: "Do pracowników firmy" },
+			{ text: "B+R" },
+			{ text: "Do innej grupy" },
+		],
+	},
+	{
+		question: "Jaki jest cel aplikacji?",
+		answers: [
+			{ text: "Zwiększenie sprzedaży" },
+			{ text: "Zwiększenie świadomości marki" },
+			{ text: "Zapewnienie rozrywki" },
+			{ text: "Edukacja" },
+			{ text: "Szkolenie" },
+			{ text: "Inny" },
+		],
+	},
+	{
+		question: "Jakie aspekty aplikacji są najważniejsze?",
+		answers: [
+			{ text: "Edukacja" },
+			{ text: "Szkolenie" },
+			{ text: "Zabawa" },
+			{ text: "Rozrywka" },
+			{ text: "Realizm" },
+			{ text: "Innowacyjność" },
+			{ text: "Wieloplatformowość" },
+			{ text: "Inny" },
+		],
+	},
+	{
+		question: "W ilu językach ma być aplikacja?",
+		answers: [{ text: "Tylko jednym" }, { text: "Więcej niż jednym" }],
+	},
+	{
+		question: "Jakie materiały do aplikacji możemy otrzymać?",
+		answers: [
+			{ text: "Modele 3D" },
+			{ text: "Projekty ekranów" },
+			{ text: "Scenariusz" },
+			{ text: "Key Visual" },
+			{ text: "Księgę znaku" },
+			{ text: "Inne" },
+		],
+	},
+	{
+		question: "Jaki jest czas na wykonanie aplikacji?",
+		answers: [
+			{ text: "Mniej niż miesiąc" },
+			{ text: "1 - 2 miesiące" },
+			{ text: "2 - 6 miesięce" },
+			{ text: "6 - 12 miesięcy" },
+			{ text: "Więcej niż 12 miesięcy" },
+			{ text: "Nie mamy konkretnego terminu" },
+		],
+	},
+	{
+		question: "Jaki jest przeznaczony budżet?",
+		answers: [
+			{ text: "Do 10 tyś." },
+			{ text: "Od 10 do 25 tyś." },
+			{ text: "Od 25 do 50 tyś." },
+			{ text: "Od 50 do 100 tyś." },
+			{ text: "Więcej niż 100 tyś." },
+			{ text: "Jeszcze nie wiemy" },
+		],
+	},
+	{
+		question: "Kiedy zaczynamy działać?",
+		answers: [
+			{ text: "W ciągu miesiąca" },
+			{ text: "Za 1 - 2 miesiące" },
+			{ text: "Za 2 - 4 miesiące" },
+			{ text: "Za 6 - 12 miesięcy" },
+			{ text: "W przyszłym roku" },
+			{ text: "Nie mamy konkretnego terminu" },
+		],
+	},
+];
+
+const encourageArr = [
+	"Świetny początek! Twoja aplikacja nabiera charakteru.",
+	"Twoje pomysły są cenne! To one sprawiają, że projekt będzie wyjątkowy",
+	"Uwzględniając wszystkie odpowiedzi to będzie innowacyjny projekt przyciągający uwagę!",
+];
+
+export { questionsMobile, encourageArr };
